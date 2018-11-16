7f614b21ff606a8b9b2a7af664749ed557430baa
Vicent R <vicentcdb@gmail.com>
2018-11-16 12:49:46 -0200

    get updates

--- src/app/model/operation.model.ts
+++ src/app/model/operation.model.ts
@@ -4,7 +4,7 @@ export class Operation{
 
     public id:number;
     public date: number;
-    public location: number;
+    public location: string;
     public produto: Produto;
     public value:number;
 }
